786bf01bbdeb267482a3fe1f9f574bc8dbab7752
tuequach <tuequach@gmail.com>
2019-03-28 18:57:56 -0500

    for loop and classes for each var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,3 +7,15 @@
   var imageHamburger = $("<img>");
   var imageHotdog = $("<img>");
   var imageFries = $("<img>");
+
+  // Arrays of values will be assign later to different images
+  var numberOptions = [5, 18, 24,];
+
+  //loop for every different food to be created to each values
+  // also adding on class for each images so it can link to CSS
+
+  for (var i = 0; i < numberOptions.length; i++) {
+    imageHamburger.addClass("hamburger-image");
+    imageHotdog.addClass("hotdog-image");
+    imageFries.addClass("fries-image");
+  }
