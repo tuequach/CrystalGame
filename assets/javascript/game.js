349e7b5e1f3595e7fba49cd1301c7423f1084f44
tuequach <tuequach@gmail.com>
2019-03-28 18:52:03 -0500

    claiming all the variables needed

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+  var targetNumber = 48;
+  $("#number-to-guess").text(targetNumber);
+
+  var counter = 0;
+
+  var imageHamburger = $("<img>");
+  var imageHotdog = $("<img>");
+  var imageFries = $("<img>");
